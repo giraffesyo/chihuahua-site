27227b2d92e4ff4d7e396fdd54adacd9f4d4518a
Michael McQuade <giraffesyo@gmail.com>
2015-08-01 22:36:35 -0500

    Initial source
    Source tree of site in progress.

---
+++ public/js/image.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  var n,
+    images = [],
+    imageData = [
+      [
+        {src: "images/breeders/baxter.jpg"},
+        {src: "images/previous/daisyduke.jpg"}
+      ]
+    ]
+};
